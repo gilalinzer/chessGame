645b3f191941d5e93163a35704d765fe3c82c249
gilalinzer <gila.linzer@gmail.com>
2022-09-11 17:02:23 -0400

    fixed deleted code from merge

--- chess_main.py
+++ chess_main.py
@@ -567,10 +567,9 @@ class ChessBoard(object):
         ans = (end_row, end_col)
 
         if ans in possible:  # the move is possible so do the move
-            # if you are getting the opponent out
-            if self.board[end_row][end_col] != None:
-                opponentPiece = self.board[end_row][end_col]
-                print(self.set_score(opponentPiece.color, opponentPiece.piece_type.value))
+            # if you are getting the opponent out, update the opponent score
+            if self.board[end_row][end_col]:
+                self.set_score(self.board[start_row][start_col].color, self.board[end_row][end_col].value)
             self.board[end_row][end_col] = self.board[start_row][start_col]
             self.board[start_row][start_col] = None
             return True
@@ -591,9 +590,10 @@ class ChessBoard(object):
             return True
         return False
 
-    def select_move(self, turn_color):
+    def find_move(self, turn_color):
         highest = 0
-        best = ()
+        target = ()
+        source = ()
 
         # let's build up a list of where all the pieces in that color are located
         sources = []
@@ -607,13 +607,24 @@ class ChessBoard(object):
             for row, col in possible:
                 spot = self.board[row][col]
                 if spot:
+                    if not target and not source:
+                        source = (spot[0], spot[1])
+                        target = (row, col)
                     if spot.piece_type.value > highest:
                         highest = spot.piece_type.value
-                        best = (row,col)
-        return best
+                        target = (row, col)
+                return source, target
 
+    def move_best_spot(self, turn_color):
+        source, target = self.find_move(turn_color)
 
+        removed = self.board[target[0]][target[1]]
 
+        if removed:
+            self.set_score(turn_color, removed.value)
+
+        self.board[target[0]][target[1]] = self.board[source[0]][source[1]]
+        self.board[source[0]][source[1]] = None
 
 
 b = ChessBoard()
